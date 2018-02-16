02460687b1263094270b2b478857c71ce008d529
Roman <rmilovanov@gmail.com>
2018-02-16 22:42:55 +0400

    Initial commit

---
+++ install.py
@@ -0,0 +1,18 @@
+import sys, os
+
+CWD = os.getcwd()       # Current user's work directory
+
+def get_imm():
+  if not os.path.isfile(CWD+"/imm.py"):
+    print "Download imm to {}".format(CWD)
+    imm_file = urllib.URLopener()
+    imm_file.retrieve("https://raw.githubusercontent.com/rmilovanov/imm/master/imm.py", CWD+"/imm.py")
+  else:
+    print "Imm already exists"
+
+def main():
+  get_imm()
+
+
+if __name__ == '__main__':
+  main()
